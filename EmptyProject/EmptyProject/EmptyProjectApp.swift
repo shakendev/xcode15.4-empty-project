e7257b364b6ebadcec6be6875db36c704af6765f
Dmitry Novikov <nvkv.dmitry@gmail.com>
2024-09-09 16:40:51 +0300

    feat: add an empty project

---
+++ EmptyProject/EmptyProject/EmptyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyProjectApp.swift
+//  EmptyProject
+//
+//  Created by Dimka Novikov on 09.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmptyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
